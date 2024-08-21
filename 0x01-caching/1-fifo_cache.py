4e452388199cc843e96dbdfe92da1343faa7387c
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-08-21 16:18:17 -0400

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -12,8 +12,6 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """adding item to cache_data"""
-        if key is None or item is None:
-            return
         if key and item:
             if self.cache_data.get(key):
                 self.toqueue.remove(key)
@@ -27,6 +25,4 @@ class FIFOCache(BaseCaching):
 
     def get(self, key):
         """gets item of given key"""
-        if key is None or key not in self.cache_data.keys:
-            return None
         return self.cache_data.get(key)
