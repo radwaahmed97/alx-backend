28844e60b33cbc58b9298762a945f9d3931cc1a5
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-08-21 15:48:45 -0400

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,37 +1,28 @@
-#!/usr/bin/env python3
-""" class FIFOCache that inherits from BaseCaching and is a caching system"""
-
-import queue
+#!/usr/bin/python3
+"""Create FIFOCache class that inherits from BaseCaching"""
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
+    """ Define FIFOCache """
+
     def __init__(self):
-        """initializing"""
+        """ Initialize FIFOCache """
+        self.queue = []
         super().__init__()
-        self.cache_data = {}
 
     def put(self, key, item):
-        """adding item to cache_data"""
-        if key is None or item is None:
-            return
-
+        """ Assign the item to the dictionary """
         if key and item:
+            if self.cache_data.get(key):
+                self.queue.remove(key)
+            self.queue.append(key)
             self.cache_data[key] = item
-
-        if len(self.cache_data.items()) > BaseCaching.MAX_ITEMS:
-            cache_data_queue = queue.Queue()
-            for key in self.cache_data.keys():
-                cache_data_queue.put(key)
-            # first item name need to be removed
-            fifo_key = cache_data_queue.get()
-            dic_items = self.cache_data.items()
-            print(f'DISCARD: {fifo_key}')
-            self.cache_data = {key: value for key, value in dic_items
-                               if key != fifo_key}
+            if len(self.queue) > self.MAX_ITEMS:
+                delete = self.queue.pop(0)
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
 
     def get(self, key):
-        """gets item of given key"""
-        if key is None or key not in self.cache_data.keys:
-            return None
+        """ Output the value associated with the given key """
         return self.cache_data.get(key)
