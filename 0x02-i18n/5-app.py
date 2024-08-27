51c41c9530e07aac082414f76c3a64dd948b1a1e
radwaahmed97 <radwa.ahmed95@yahoo.com>
2024-08-27 17:53:13 -0400

    5-app.py

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -57,7 +57,7 @@ def get_locale():
 @basic_app.route('/')
 def welcome_hello_world():
     """basic app"""
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
